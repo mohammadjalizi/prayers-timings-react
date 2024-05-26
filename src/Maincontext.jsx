98fb357dd580c16d194b5d9d4a06fb037d179d17
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-26 07:49:28 +0330

    start

---
+++ src/Maincontext.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Maincontext = () => {
+  return (
+    <div className=' flex justify-center items-center '>
+        
+
+<div>
+<h2>   4.23 |سبتمبر2024 9</h2>
+<h1>مکه المکرمه </h1>
+</div>
+
+<div>
+<h2>    متبقي حتى صلاة العصر</h2>
+<h1> 00:10:20 </h1>
+</div>
+
+
+    </div>
+  )
+}
+
+export default Maincontext
