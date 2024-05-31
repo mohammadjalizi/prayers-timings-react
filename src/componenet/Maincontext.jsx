7e461b199951d769af72170fc4eb48f58cba4c18
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-31 15:03:26 +0330

    add  font

--- src/componenet/Maincontext.jsx
+++ src/componenet/Maincontext.jsx
@@ -34,6 +34,18 @@ const Maincontext = () => {
 <Prayer name="المغرب" time="07:44"  image="src/assets/night-prayer-mosque.png" />
 </div>
 
+
+{/* select */}
+<form class="max-w-sm mx-auto">
+  <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+  <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    <option selected>المدینه</option>
+    <option value="US">United States</option>
+ 
+  </select>
+</form>
+
+
 </div>
   )
 }
