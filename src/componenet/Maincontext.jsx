376b0435630de45bd49a24d1a0fba9521f27c6dd
mohammadjalizi <mjalizi1376@gmail.com>
2024-05-31 16:23:37 +0330

    install axios

--- src/componenet/Maincontext.jsx
+++ src/componenet/Maincontext.jsx
@@ -3,16 +3,6 @@ import Prayer from './Prayer'
 import axios from 'axios'
 const Maincontext = () => {
 
-  const getTimings=async()=>{
-const data= await axios.get("https://api.aladhan.com/v1/timingsByCity/31-05-2024?city=Dubai&country=United+Arab+Emirates&method=8")
-console.log(data.data.data.timings)
-  }
-useEffect(()=>{
-
-  getTimings()
-
-},[])
-
   const[timings,Settimings]=useState({
 
 
@@ -23,6 +13,28 @@ useEffect(()=>{
 		Isha: "19:33",
 
   })
+const[selectedcity,Setselected]=useState("الریاض")
+  const getTimings=async()=>{
+const data= await axios.get("https://api.aladhan.com/v1/timingsByCity/31-05-2024?city=Dubai&country=United+Arab+Emirates&method=8");
+
+Settimings(Response.data.data.timings)
+console.log(data.data.data.timings)
+  }
+useEffect(()=>{
+
+  getTimings()
+
+},[])
+
+
+const handelcity=(event)=>{
+
+
+console.log("the new value",event.target.value)
+Setselected(event.target.value)
+
+}
+
   return (
 <div className=' '>
         <div className='  flex justify-between gap-44 '>
@@ -30,7 +42,7 @@ useEffect(()=>{
     
     <div>
     <h2>   4.23 |سبتمبر2024 9</h2>
-    <h1 className=' text-3xl '>مکه المکرمه </h1>
+    <h1 className=' text-3xl '> {selectedcity}</h1>
     </div>
     
     <div>
@@ -59,10 +71,11 @@ useEffect(()=>{
 {/* select */}
 <form class="max-w-sm mx-auto  ">
   <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-  <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+  <select onChange={handelcity} id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     <option selected>المدینه</option>
-    <option value="US">United States</option>
- 
+    <option value="riath">الریاض</option>
+    <option value="Damam">الدمام</option>
+    <option value="ahwaz">الاهواز</option>
   </select>
 </form>
 
